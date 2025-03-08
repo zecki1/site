1d44eba5ce8dd32f07812a00c446400db375325a
Ezequiel Regino <ezequiel.rmoncao@sp.senai.br>
2025-03-08 09:44:14 -0300

    Atualização da reader e criação do readme.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "../app/globals.css"
-import { ThemeProvider } from "@/components/ThemeProvider"
-import { Header } from "@/components/Header"
+import { ThemeProvider } from "@/components/layout/ThemeProvider"
+import { Header } from "@/components/layout/Header"
 import { Toaster } from "@/components/ui/sonner"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

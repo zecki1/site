c9f0d956d39bebf59e468b652b83f12be0c225ff
Ezequiel Regino Monção <zecki1@hotmail.com>
2025-06-05 18:40:37 -0300

    ajuste no erro do tailwind!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,12 @@
-// tailwind.config.ts
-import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
+        './src/app/**/*.{js,ts,jsx,tsx}',
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
     ],
-    darkMode: "class",
     theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-                primary: {
-                    DEFAULT: "var(--primary)",
-                    foreground: "var(--primary-foreground)",
-                },
-                border: "var(--border)",
-            },
-        },
+        extend: {},
     },
-    plugins: [animate],
-} satisfies Config;
+    plugins: [],
+};
